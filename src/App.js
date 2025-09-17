6ba7d88d66970308d1be1fd7398372dfb753d2b5
lavanyakusumanchi <lavanyakusumanchi22@gmail.com>
2025-09-17 11:27:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Greeting from './Greeting';
+
+function App() {
+  return (
+    <div>
+      <h1>Props Demo</h1>
+      <Greeting message="Hello from the Parent Component!" />
+    </div>
+  );
+}
+
+export default App;
